ff20bd2a7eb50609ce10257e9ce00475ec1f64d4
James Maynard <jameskirkmaynard@gmail.com>
2015-02-24 12:03:04 -0800

    Add studentRoster factory and PermissionSlip controller.

--- controllers/FieldTripController.js
+++ controllers/FieldTripController.js
@@ -1,3 +1,6 @@
-studentRoster.controller('FieldTripCtrl', function FieldTripCtrl($scope, ) {
-  $scope.studentsWithPermission = [{ name: "Jane Doe" }];
+studentRoster.controller('FieldTripCtrl', function FieldTripCtrl($scope, StudentsFactory) {
+  $scope.students = StudentsFactory.students;
+  $scope.addStudentWithPermissionSlip = function(student) {
+    student.permissionSlip = true;
+  };
 });
